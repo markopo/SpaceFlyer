9eb8086cd6cb05b8436ddb7579ce0030ea068d19
Marko Poikkimäki <marko_poikkimaki@hotmail.com>
2019-02-24 12:05:56 +0100

    Game loop

--- SpaceFlyer/GameScene.swift
+++ SpaceFlyer/GameScene.swift
@@ -10,11 +10,19 @@ import SpriteKit
 import GameplayKit
 
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
     private let player = SKSpriteNode(imageNamed: "player-rocket.png")
     private var touchingPlayer = false
     private var gameTimer: Timer?
+    private let scoreLabel = SKLabelNode(fontNamed: "AvenirNextCondensed-Bold")
+    private let gameOverLabel = SKLabelNode(fontNamed: "AvenirNextCondensed-Bold")
+    
+    var score = 0 {
+        didSet {
+              scoreLabel.text = "Score: \(score)"
+        }
+    }
     
     
     private func addBackground() {
@@ -34,12 +42,35 @@ class GameScene: SKScene {
     private func addPlayer() {
         player.position.x = -player.size.width
         player.zPosition = 1
+        player.physicsBody = SKPhysicsBody(texture: player.texture!, size: player.size)
+        player.physicsBody?.categoryBitMask = 1
+        player.physicsBody?.affectedByGravity = false
         addChild(player)
+        
+        physicsWorld.contactDelegate = self
+    }
+    
+    private func addScoreLabel() {
+        scoreLabel.zPosition = 2
+        scoreLabel.position.y = 300
+        score = 0
+        addChild(scoreLabel)
+    }
+    
+    private func gameOver() {
+        gameOverLabel.text = "GAME OVER!"
+        gameOverLabel.name = "game_over"
+        gameOverLabel.position.x = 0
+        gameOverLabel.position.y = 0
+        gameOverLabel.zPosition = 3
+        addChild(gameOverLabel)
+        
+        
     }
     
     @objc
     private func createAsteroid() {
-        let randomDistribution = GKRandomDistribution(lowestValue: -200, highestValue: 200)
+        let randomDistribution = GKRandomDistribution(lowestValue: -100, highestValue: 100)
         let asteroid = SKSpriteNode(imageNamed: "asteroid")
         asteroid.position = CGPoint(x: 350, y: randomDistribution.nextInt())
         asteroid.name = "asteroid"
@@ -50,17 +81,45 @@ class GameScene: SKScene {
         asteroid.physicsBody?.velocity = CGVector(dx: -100, dy: 0)
         asteroid.physicsBody?.linearDamping = 0
         asteroid.physicsBody?.affectedByGravity = false
+        asteroid.physicsBody?.contactTestBitMask = 1
+        asteroid.physicsBody?.categoryBitMask = 0
+        
+        createEnergy()
     }
     
-    override func didMove(to view: SKView) {
+    @objc
+    private func createEnergy() {
+        let randomDistribution = GKRandomDistribution(lowestValue: -300, highestValue: 300)
+        let energy = SKSpriteNode(imageNamed: "energy")
+        energy.position = CGPoint(x: 350, y: randomDistribution.nextInt())
+        energy.name = "energy"
+        energy.zPosition = 1
+        addChild(energy)
+        
+        energy.physicsBody = SKPhysicsBody(texture: energy.texture!, size: energy.size)
+        energy.physicsBody?.velocity = CGVector(dx: -100, dy: 0)
+        energy.physicsBody?.linearDamping = 0
+        energy.physicsBody?.affectedByGravity = false
+        energy.physicsBody?.contactTestBitMask = 1
+        energy.physicsBody?.categoryBitMask = 0
+    }
+    
+    func startGame() {
         addBackground()
         addParticles()
         addPlayer()
+        addScoreLabel()
         
         gameTimer = Timer.scheduledTimer(timeInterval: 1.35, target: self, selector: #selector(createAsteroid), userInfo: nil, repeats: true)
     }
     
+    override func didMove(to view: SKView) {
+        startGame()
+    }
+    
     func touchDown(atPoint pos : CGPoint) {
+      
+        
         
     }
     
@@ -79,6 +138,11 @@ class GameScene: SKScene {
         
         if tappedNodes.contains(player) {
             touchingPlayer = true
+        }
+        
+        if(tappedNodes.contains(gameOverLabel)) {
+            removeAllChildren();
+            startGame()
         }
     }
     
@@ -103,7 +167,34 @@ class GameScene: SKScene {
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
         
-       
+    }
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        guard let nodeA = contact.bodyA.node else { return }
+        guard let nodeB = contact.bodyB.node else { return }
         
+        
+        if nodeA == player && nodeB.name == "asteroid" {
+            playerHit(node: nodeB)
+        }
+        else if(nodeA.name == "asteroid" && nodeB == player){
+            playerHit(node: nodeA)
+        }
+      
+        
+        if(nodeA == player && nodeB.name == "energy"){
+            score += 1
+            nodeB.removeFromParent()
+        }
+        else if(nodeA.name == "energy" && nodeB == player) {
+            score += 1
+            nodeA.removeFromParent()
+        }
+        
+    }
+    
+    func playerHit(node: SKNode) {
+        player.removeFromParent()
+        gameOver()
     }
 }
